c19287616976208ae0ba89e42f297646ffbd9757
abgbro <codepath@abhigarlapati.com>
2024-03-23 17:37:37 -0400

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by user256510 on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
